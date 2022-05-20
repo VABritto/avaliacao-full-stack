4e9449dca970e72cacbd8c4b238c458758086f3a
Victor Britto <valencarbritto@gmail.com>
2022-05-19 22:02:59 -0300

    Initial Back-End structure

---
+++ avaliacao-full-stack-back/src/main/java/br/com/financeiro/avaliacaofullstackback/AvaliacaoFullStackApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeiro.avaliacaofullstackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoFullStackApplication.class, args);
+	}
+
+}
---
+++ avaliacao-full-stack-back/src/test/java/br/com/financeiro/avaliacaofullstackback/AvaliacaoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financeiro.avaliacaofullstackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
